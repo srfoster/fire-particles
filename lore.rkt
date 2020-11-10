480dba8dd1ee0b3a3d95ef8cec088bdf0820e7a6
srfoster <lindseyhandley@gmail.com>
2020-11-10 08:51:48 -0800

    reorg

--- lore.rkt
+++ lore.rkt
@@ -3,51 +3,48 @@
 (require fire-particles/main
 	 website-js)
 
+(provide name
+         description
+         runes
+         )
+
 ;Each rune has lore... (Could pop it up in game when you collect one)
 ;The whole mod has lore... (Combines all into a cohesive story)
 
-(define (lore)
-  (page index.html
-	(content
-	  (container
-	    @md{
-            # Illusions of Flame
+(define (name)
+  "Illusions of Flame"
+  )
 
-	    The @b{Illusions of Flame} Rune Collection was crafted by @b{Firebeard the Great} in the early days of the @b{Dark Invasion.}  They were instrumental in turning the tide, at least for a while.
+(define (description)
+  @md{
+  The @b{Illusions of Flame} Rune Collection was crafted by @b{Firebeard the Great} in the early days of the @b{Dark Invasion.}  They were instrumental in turning the tide, at least for a while.
 
-	    The Collection was thought to be lost for many years.  Recently, however, I (@b{Stephen the Scrivener}) stumbled across @b{Firebeard}'s old tomes and have been working diligently to reconstruct the original Runes. 
+  The Collection was thought to be lost for many years.  Recently, however, I (@b{Stephen the Scrivener}) stumbled across @b{Firebeard}'s old tomes and have been working diligently to reconstruct the original Runes. 
 
-	    These are my field notes.
+  These are my field notes.
+  })
 
+(define (runes)
+  (list
+    @md{
+    ## Flames Rune
 
-	    @md{
-	    ## Flames Rune
+    @(flames-rune)}
 
-	    @(flames-rune)
-	    }
+    @md{
+    ## Fire Beam Rune
 
-	    @md{
-	    ## Fire Beam Rune
+    @(fire-beam-rune)}
 
-	    @(fire-beam-rune)
-	    }
+    @md{ 
+    ## Explosion Rune
 
-	    @md{
-	    ## Explosion Rune
+    @(explosion-rune)}
 
-	    @(explosion-rune)
-	    }
+    )
+  )
 
-	    [TODO: Screenshots?]
-
-	    [Note: These runes images 
-		   are subject to change.  ]
-	    [Meta note: How to we note if something is subject to change?]
-	    [Meta note: What do we want rune sto look like?]
-
-	    }))))
-
-
+#;
 (module+ main
 	 (render #:to "out" 
 		 (list
