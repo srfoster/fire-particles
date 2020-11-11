cf22e7486d6bb1b2138b33f7b86d5eb1feadfba2
Stephen Foster <stephen@thoughtstem.com>
2020-11-11 11:42:53 -0800

    lore tweaks

--- lore.rkt
+++ lore.rkt
@@ -9,7 +9,7 @@
   @md{
   The @b{Illusions of Flame} Rune Collection was crafted by @b{Firebeard the Great} in the early days of the @b{Dark Invasion.}  They were instrumental in turning the tide, at least for a while.
 
-  The Collection was thought to be lost for many years.  Recently, however, I (@b{Stephen the Scrivener}) stumbled across @b{Firebeard}'s old tomes and have been working diligently to reconstruct the original Runes. 
+  The Collection was thought to be lost for many years.  But recently, I -- @b{Stephen the Scrivener} -- stumbled across @b{Firebeard}'s old tomes and have been working diligently to reconstruct the original Runes. 
 
   These are my field notes.
   }
